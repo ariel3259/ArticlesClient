06c875a7fc33d3ce4498cdbc303be0cf8b3427a7
Ariel_Santangelo <arielsantangelo610@gmail.com>
2022-09-06 10:11:38 -0300

    crud completado

--- src/page/Articles.vue
+++ src/page/Articles.vue
@@ -25,12 +25,12 @@
             ],
             nombre: '',
             precio: 0,
-            stock: 0
+            stock: 0,
+            id: 0
         };
     },
     methods: {
-        save(event){
-            event.preventDefault();
+        save(){
             let article = {
                 id: this.articles.length + 1,
                 nombre: this.nombre,
@@ -42,14 +42,19 @@
         deleteArticle(id){
             this.articles = this.articles.filter(element => element.id !== id);
         },
-        modifyArticle(article){
- 
+        modify(){
+            let index = `${this.id - 1}`;
+            this.articles[index].nombre = this.nombre;
+            this.articles[index].precio = this.precio;
+            this.articles[index].stock = this.stock;
+            this.id = 0;
         },
         togleModifyArticle(id){
             let article = this.articles.filter(element => element.id === id);
             this.nombre = article[0].nombre;
             this.precio = article[0].precio;
             this.stock = article[0].stock;
+            this.id = id;
         }
     },
     components: { Table }
@@ -58,7 +63,7 @@
 </script>
 
 <template>
-    <form class="form-control p-5 m-3" @submit="save($event)">
+    <form class="form-control p-5 m-3" @submit.prevent="() => id === 0 ? save() : modify()">
         <div class="mb-3">
             <label class="form-label" for="nombre">Nombre</label>
             <input type="text" class="form-control" id="nombre" v-model="nombre">
@@ -71,7 +76,8 @@
             <label class="form-label" for="stock">Stock</label>
             <input type="number" class="form-control" id="stock" v-model="stock">
         </div>
-        <button class="btn btn-primary">Crear articulo</button>
+        <button type="submit" class="btn btn-primary">{{id === 0 ? 'Crear articulo' : 'Modificar articulo'}}</button>
+        <button type="button" class="btn btn-secondary" v-show="id !== 0" @click="() => id = 0">Cancelar</button>
     </form>
     <Table :fields="Object.keys({id: 0, nombre: '', precio: 0, stock: 0})" :togleModifyItem="togleModifyArticle" :deleteItem="deleteArticle" :data="articles" />
 </template>
